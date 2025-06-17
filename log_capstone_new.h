d10b909ede897b48d4af12c58accf8852799f716
Nop_one <68745798+xkp95175333@users.noreply.github.com>
2025-06-18 04:46:37 +0700

    Update log_capstone_new.h

--- log_capstone_new.h
+++ log_capstone_new.h
@@ -1568,4 +1568,214 @@ PS C:\Users\0ANew\source\repos>
 
 
 
+    Rebuild started at 4:48...
+1>------ Rebuild All started: Project: ZERO_CHECK, Configuration: Release x64 ------
+1>1>Checking Build System
+2>------ Rebuild All started: Project: capstone, Configuration: Release x64 ------
+2>Building Custom Rule E:/ProjectTry/capstone6/CMakeLists.txt
+2>cs.c
+2>Mapping.c
+2>MCInst.c
+2>MCInstrDesc.c
+2>MCInstPrinter.c
+2>MCRegisterInfo.c
+2>SStream.c
+2>utils.c
+2>ARMBaseInfo.c
+2>ARMDisassembler.c
+2>ARMDisassemblerExtension.c
+2>ARMInstPrinter.c
+2>ARMMapping.c
+2>ARMModule.c
+2>AArch64BaseInfo.c
+2>AArch64Disassembler.c
+2>E:\ProjectTry\capstone6\arch\ARM\ARMMapping.c(619,19): warning C5287: operands are different enum types 'arm_op_type' and 'cs_op_type'; use an explicit cast to silence this warning
+2>E:\ProjectTry\capstone6\arch\ARM\ARMMapping.c(621,24): warning C5287: operands are different enum types 'arm_op_type' and 'cs_op_type'; use an explicit cast to silence this warning
+2>AArch64DisassemblerExtension.c
+2>AArch64InstPrinter.c
+2>AArch64Mapping.c
+2>AArch64Module.c
+2>MipsDisassembler.c
+2>MipsInstPrinter.c
+2>MipsMapping.c
+2>MipsModule.c
+2>PPCDisassembler.c
+2>PPCInstPrinter.c
+2>PPCMapping.c
+2>PPCModule.c
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64Disassembler.c(1948,53): warning C4133: 'function': incompatible types - from 'const aarch64_insn_group [3]' to 'const unsigned int *'
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64Disassembler.c(1973,53): warning C4133: 'function': incompatible types - from 'const aarch64_insn_group [3]' to 'const unsigned int *'
+2>E:\ProjectTry\capstone6\arch\Mips\MipsInstPrinter.c(205,3): warning C4098: 'printJumpOperand': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsInstPrinter.c(218,3): warning C4098: 'printBranchOperand': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\PowerPC\PPCPredicates.h(49,27): warning C5287: operands are different enum types 'ppc_pred' and 'ppc_br_hint'; use an explicit cast to silence this warning
+2>(compiling source file '../arch/PowerPC/PPCInstPrinter.c')
+2>E:\ProjectTry\capstone6\arch\PowerPC\PPCPredicates.h(55,27): warning C5287: operands are different enum types 'ppc_pred' and 'ppc_br_hint'; use an explicit cast to silence this warning
+2>(compiling source file '../arch/PowerPC/PPCInstPrinter.c')
+2>E:\ProjectTry\capstone6\arch\PowerPC\PPCPredicates.h(49,27): warning C5287: operands are different enum types 'ppc_pred' and 'ppc_br_hint'; use an explicit cast to silence this warning
+2>(compiling source file '../arch/PowerPC/PPCDisassembler.c')
+2>E:\ProjectTry\capstone6\arch\PowerPC\PPCPredicates.h(55,27): warning C5287: operands are different enum types 'ppc_pred' and 'ppc_br_hint'; use an explicit cast to silence this warning
+2>(compiling source file '../arch/PowerPC/PPCDisassembler.c')
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(389,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(391,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(393,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>X86Disassembler.c
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(395,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(397,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(399,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(401,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(403,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>X86DisassemblerDecoder.c
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(405,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(407,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(409,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(411,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(413,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(415,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(417,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(419,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(421,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(423,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(425,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(427,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(429,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(431,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(433,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(435,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(437,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(439,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(441,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(445,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>E:\ProjectTry\capstone6\arch\Mips\MipsMapping.c(447,3): warning C4098: 'Mips_add_cs_detail': 'void' function returning a value
+2>X86IntelInstPrinter.c
+2>X86InstPrinterCommon.c
+2>X86Mapping.c
+2>X86Module.c
+2>X86ATTInstPrinter.c
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64InstPrinter.c(727,48): warning C4133: 'function': incompatible types - from 'const aarch64_insn_group [3]' to 'const unsigned int *'
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64InstPrinter.c(754,26): warning C4133: 'function': incompatible types - from 'const aarch64_insn_group [3]' to 'const unsigned int *'
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64InstPrinter.c(776,26): warning C4133: 'function': incompatible types - from 'const aarch64_insn_group [3]' to 'const unsigned int *'
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64InstPrinter.c(798,17): warning C4133: 'function': incompatible types - from 'const aarch64_insn_group [3]' to 'const unsigned int *'
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64InstPrinter.c(867,17): warning C4133: 'function': incompatible types - from 'const aarch64_insn_group [3]' to 'const unsigned int *'
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64InstPrinter.c(1456,1): warning C4133: 'function': incompatible types - from 'const aarch64_insn_group [3]' to 'const unsigned int *'
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64InstPrinter.c(1457,1): warning C4133: 'function': incompatible types - from 'const aarch64_insn_group [3]' to 'const unsigned int *'
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64InstPrinter.c(2154,36): warning C4133: 'function': incompatible types - from 'const aarch64_insn_group [3]' to 'const unsigned int *'
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64InstPrinter.c(2248,20): warning C4133: 'function': incompatible types - from 'const aarch64_insn_group [3]' to 'const unsigned int *'
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64InstPrinter.c(2252,17): warning C4133: 'function': incompatible types - from 'const aarch64_insn_group [3]' to 'const unsigned int *'
+2>SparcDisassembler.c
+2>SparcInstPrinter.c
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64Mapping.c(309,48): warning C5287: operands are different enum types 'aarch64_op_type' and 'cs_op_type'; use an explicit cast to silence this warning
+2>SparcMapping.c
+2>SparcModule.c
+2>SystemZDisassembler.c
+2>SystemZDisassemblerExtension.c
+2>SystemZInstPrinter.c
+2>SystemZMapping.c
+2>SystemZModule.c
+2>SystemZMCTargetDesc.c
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64Mapping.c(1559,11): warning C4133: 'function': incompatible types - from 'const aarch64_insn_group [3]' to 'const unsigned int *'
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64Mapping.c(1696,21): warning C4133: 'function': incompatible types - from 'const aarch64_insn_group [3]' to 'const unsigned int *'
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64Mapping.c(1704,18): warning C4133: 'function': incompatible types - from 'const aarch64_insn_group [3]' to 'const unsigned int *'
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64Mapping.c(1306,8): warning C5286: implicit conversion from enum type 'aarch64_op_type' to enum type 'cs_op_type'; use an explicit cast to silence this warning
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64Mapping.c(1310,8): warning C5286: implicit conversion from enum type 'aarch64_op_type' to enum type 'cs_op_type'; use an explicit cast to silence this warning
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64Mapping.c(1314,8): warning C5286: implicit conversion from enum type 'aarch64_op_type' to enum type 'cs_op_type'; use an explicit cast to silence this warning
+2>E:\ProjectTry\capstone6\arch\AArch64\AArch64Mapping.c(1944,15): warning C4133: 'function': incompatible types - from 'const aarch64_insn_group [3]' to 'const unsigned int *'
+2>XCoreDisassembler.c
+2>XCoreInstPrinter.c
+2>XCoreMapping.c
+2>XCoreModule.c
+2>M68KDisassembler.c
+2>M68KInstPrinter.c
+2>M68KModule.c
+2>TMS320C64xDisassembler.c
+2>TMS320C64xInstPrinter.c
+2>TMS320C64xMapping.c
+2>TMS320C64xModule.c
+2>M680XDisassembler.c
+2>M680XInstPrinter.c
+2>M680XModule.c
+2>EVMDisassembler.c
+2>EVMInstPrinter.c
+2>EVMMapping.c
+2>EVMModule.c
+2>WASMDisassembler.c
+2>WASMInstPrinter.c
+2>WASMMapping.c
+2>WASMModule.c
+2>MOS65XXModule.c
+2>MOS65XXDisassembler.c
+2>BPFDisassembler.c
+2>BPFInstPrinter.c
+2>BPFMapping.c
+2>BPFModule.c
+2>RISCVDisassembler.c
+2>RISCVInstPrinter.c
+2>RISCVMapping.c
+2>RISCVModule.c
+2>SHDisassembler.c
+2>SHInstPrinter.c
+2>SHModule.c
+2>TriCoreDisassembler.c
+2>TriCoreInstPrinter.c
+2>TriCoreMapping.c
+2>TriCoreModule.c
+2>AlphaDisassembler.c
+2>AlphaInstPrinter.c
+2>AlphaMapping.c
+2>AlphaModule.c
+2>HPPADisassembler.c
+2>HPPAInstPrinter.c
+2>HPPAMapping.c
+2>HPPAModule.c
+2>LoongArchDisassembler.c
+2>LoongArchDisassemblerExtension.c
+2>LoongArchInstPrinter.c
+2>LoongArchMapping.c
+2>LoongArchModule.c
+2>XtensaDisassembler.c
+2>XtensaInstPrinter.c
+2>XtensaMapping.c
+2>XtensaModule.c
+2>capstone.vcxproj -> E:\ProjectTry\capstone6\build\Release\capstone.lib
+2>Done building project "capstone.vcxproj".
+3>------ Rebuild All started: Project: fuzz_disasm, Configuration: Release x64 ------
+4>------ Rebuild All started: Project: cstool, Configuration: Release x64 ------
+3>Building Custom Rule E:/ProjectTry/capstone6/CMakeLists.txt
+4>Building Custom Rule E:/ProjectTry/capstone6/CMakeLists.txt
+3>onefile.c
+4>cstool.c
+3>fuzz_disasm.c
+3>platform.c
+4>cstool_aarch64.c
+3>Generating Code...
+4>cstool_alpha.c
+3>fuzz_disasm.vcxproj -> E:\ProjectTry\capstone6\build\Release\fuzz_disasm.lib
+4>cstool_arm.c
+4>cstool_bpf.c
+4>cstool_evm.c
+4>cstool_hppa.c
+4>cstool_loongarch.c
+4>cstool_m680x.c
+4>cstool_m68k.c
+4>cstool_mips.c
+4>cstool_mos65xx.c
+4>cstool_powerpc.c
+4>cstool_riscv.c
+4>cstool_sh.c
+4>cstool_sparc.c
+4>cstool_systemz.c
+4>cstool_tms320c64x.c
+4>cstool_tricore.c
+4>cstool_wasm.c
+4>Generating Code...
+4>Compiling...
+4>cstool_x86.c
+4>cstool_xcore.c
+4>cstool_xtensa.c
+4>getopt.c
+4>Generating Code...
+4>cstool.vcxproj -> E:\ProjectTry\capstone6\build\Release\cstool.lib
+========== Rebuild All: 4 succeeded, 0 failed, 0 skipped ==========
+========== Rebuild completed at 4:48 and took 10.344 seconds ==========
+
+
 
